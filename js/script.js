b68480468d731b8ecb93b213c6524b4f7ed64e5a
n_meeks <najja.meeks3@gmail.com>
2026-01-30 05:27:54 -0800

    Updated image/styles/name and added JS functions

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+const createPet = function (name, species) {
+    const pet = {
+       name: name,
+       species: species,
+       isTired: 5, 
+       sleep: function () {
+           console.log(`${this.name} needs a nap. Zzz...`)
+           this.isTired = 1;
+       },
+       play: function () {
+        if (this.isTired === 10) {
+         console.log("Too tired to play.");
+         this.sleep();
+        } else {
+         console.log(`Yay! ${this.name} loves to play!`);
+         this.isTired += 1;
+        }
+       } 
+     };
+   return pet;
+};
+const una = createPet("Una", "kitten");
+const honey = createPet("Honey", "rabbit");
+const kevin = createPet("Kevin", "bearded dragon");
+const bailey = createPet("Bailey", "dog");
+const pepper = createPet("Pepper", "leopard gecko");
+
+// console.log(una, honey, kevin, bailey, pepper);
+honey.sleep();
+pepper.play();
+console.log(honey, pepper);
+
