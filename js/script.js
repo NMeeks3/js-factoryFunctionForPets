53a7a03f8e5ed6fc9341b156068bc451a3401e5f
n_meeks <najja.meeks3@gmail.com>
2026-02-02 22:28:02 -0800

    Added display function and click event

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const createPet = function (name, species) {
        species: species,
        isTired: 5, 
        sleep: function () {
-           console.log(`${this.name} needs a nap. Zzz...`)
+           console.log(`${this.name} needs a nap. Zzz...`);
            this.isTired = 1;
        },
        play: function () {
@@ -28,7 +28,42 @@ const bailey = createPet("Bailey", "dog");
 const pepper = createPet("Pepper", "leopard gecko");
 
 // console.log(una, honey, kevin, bailey, pepper);
-honey.sleep();
-pepper.play();
-console.log(honey, pepper);
+// honey.sleep();
+// pepper.play();
+// console.log(honey, pepper);
+
+una.isTired = 8;
+kevin.isTired = 9;
+
+const allPets = [
+  una,
+  honey,
+  kevin,
+  bailey,
+  pepper
+];
+
+console.log(allPets);
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+
+  for (let pet of petArray) {
+    let status = "ready to play!";
+    if (pet.isTired >= 7) {
+      status = "sleeping.";
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+    pets.append(li);
+  }
+};
+statusButton.addEventListener("click", function () {
+  showPets(allPets);
+  }
+);
+
+
+
+
 
